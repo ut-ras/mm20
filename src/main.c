55b358d66bb1a662bf2e9d4778d53196027665a7
Vivek Malneedi <vivekmalneedi@gmail.com>
2019-11-02 20:47:30 -0500

    adc output consumable by serial plotter

--- src/main.c
+++ src/main.c
@@ -35,15 +35,10 @@ void blink_success(void) {
 void timer0_init() {
   ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0);
   ROM_TimerConfigure(TIMER0_BASE, TIMER_CFG_PERIODIC);
-  ROM_TimerLoadSet(TIMER0_BASE, TIMER_A, 800000); // 100Hz
+  ROM_TimerLoadSet(TIMER0_BASE, TIMER_A, 1600000); // 100Hz
   ROM_IntEnable(INT_TIMER0A);
   ROM_TimerIntEnable(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
   ROM_TimerEnable(TIMER0_BASE, TIMER_BOTH);
-}
-void timer0a_handler(void) {
-  ROM_TimerIntClear(TIMER0_BASE, TIMER_A);
-  ROM_GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_7,
-                   ~ROM_GPIOPinRead(GPIO_PORTA_BASE, GPIO_PIN_7));
 }
 
 void adc0_init() {
@@ -67,8 +62,15 @@ void adc0_read() {
   }
   ROM_ADCIntClear(ADC0_BASE, 3);
   ROM_ADCSequenceDataGet(ADC0_BASE, 3, pui32ADC0Value); // read AIN0 E3
-  printf("AIN0 = %4d\n\r", pui32ADC0Value[0]);
-  // printf("AIN0\n\r");
+}
+
+void timer0a_handler(void) {
+  ROM_TimerIntClear(TIMER0_BASE, TIMER_A);
+  ROM_GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_7,
+                   ~ROM_GPIOPinRead(GPIO_PORTA_BASE, GPIO_PIN_7));
+
+  adc0_read();
+  printf("%4d \n\r", pui32ADC0Value[0]);
 }
 
 int main(void) {
@@ -81,7 +83,7 @@ int main(void) {
   adc0_init();
   while (1) {
     blink_success();
-    adc0_read();
+    blink_success();
     wait_ms(1000);
   }
 }
