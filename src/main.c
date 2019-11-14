12fc04daabc29124e59b3456a6583f4b500ab365
Vivek Malneedi <vivekmalneedi@gmail.com>
2019-11-14 17:31:46 -0600

    pwm test code

--- src/main.c
+++ src/main.c
@@ -6,6 +6,8 @@
 #include <tivaware/gpio.h>
 #include <tivaware/hw_ints.h>
 #include <tivaware/hw_memmap.h>
+#include <tivaware/pin_map.h>
+#include <tivaware/pwm.h>
 #include <tivaware/rom.h>
 #include <tivaware/sysctl.h>
 #include <tivaware/timer.h>
@@ -54,6 +56,21 @@ void adc0_init() {
   ROM_ADCIntClear(ADC0_BASE, 3);
 }
 
+void pwm0_init(int freq, int duty) {
+  ROM_SysCtlPWMClockSet(SYSCTL_PWMDIV_8);
+  ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_PWM0);
+  ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
+  ROM_GPIOPinConfigure(GPIO_PB6_M0PWM0);
+  ROM_GPIOPinTypePWM(GPIO_PORTB_BASE, GPIO_PIN_6);
+  ROM_PWMGenConfigure(PWM0_BASE, PWM_GEN_0,
+                      PWM_GEN_MODE_UP_DOWN | PWM_GEN_MODE_NO_SYNC);
+  ROM_PWMGenPeriodSet(PWM0_BASE, PWM_GEN_0, 100000);
+  ROM_PWMPulseWidthSet(PWM0_BASE, PWM_OUT_0,
+                       ROM_PWMGenPeriodGet(PWM0_BASE, PWM_GEN_0) / duty);
+  ROM_PWMOutputState(PWM0_BASE, PWM_OUT_0_BIT, true);
+  ROM_PWMGenEnable(PWM0_BASE, PWM_GEN_0);
+}
+
 static uint32_t pui32ADC0Value[1];
 void adc0_read() {
   ROM_ADCProcessorTrigger(ADC0_BASE, 3); // trigger ADC conversion
@@ -68,7 +85,6 @@ void timer0a_handler(void) {
   ROM_TimerIntClear(TIMER0_BASE, TIMER_A);
   ROM_GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_7,
                    ~ROM_GPIOPinRead(GPIO_PORTA_BASE, GPIO_PIN_7));
-
   adc0_read();
   printf("%4d \n\r", pui32ADC0Value[0]);
 }
@@ -79,11 +95,13 @@ int main(void) {
   ROM_SysTickEnable();
   launchpad_init();
   ROM_GPIOPinTypeGPIOOutput(GPIO_PORTA_BASE, GPIO_PIN_7);
-  timer0_init();
-  adc0_init();
+  // timer0_init();
+  // adc0_init();
+  pwm0_init(100, 2);
   while (1) {
     blink_success();
-    blink_success();
-    wait_ms(1000);
+
+    printf("%d\n\r", ROM_PWMGenPeriodGet(PWM0_BASE, PWM_GEN_0));
+    wait_ms(100);
   }
 }
